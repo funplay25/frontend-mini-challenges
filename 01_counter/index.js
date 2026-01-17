74a25a90c4f8a1316f6add117af44b78e47addfb
Pranav <bizzareforyou@gmail.com>
2026-01-17 10:18:32 +0530

    fix logic in index.js of 01_counter

--- 01_counter/index.js
+++ 01_counter/index.js
@@ -9,22 +9,16 @@ let count = 0;
 counterPara.innerHTML = count;
 
 decreaseBtn.addEventListener("click", () => {
-  console.log(count);
-  count = count - increaseBy.value;
+  count -= Number(increaseBy.value);
   counterPara.innerHTML = count;
 });
 
 increaseBtn.addEventListener("click", () => {
-  count += +increaseBy.value;
+  count += Number(increaseBy.value);
   counterPara.innerHTML = count;
 });
 
 resetBtn.addEventListener("click", () => {
   count = 0;
   counterPara.innerHTML = count;
-});
-
-increaseBy.addEventListener("change", (e) => {
-  count = +e.target.value;
-  console.log(count);
 });
