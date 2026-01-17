64f309aa1d2cf8327db796dc10ae6864885bb83c
Pranav <bizzareforyou@gmail.com>
2026-01-17 10:19:23 +0530

    give final logic to 02_guess_the_number

--- 02_guess_the_number/index.js
+++ 02_guess_the_number/index.js
@@ -3,26 +3,38 @@ const submitBtn = document.querySelector(".submit-btn");
 const newgamebtn = document.querySelector(".newgame-btn");
 const resultTextDiv = document.querySelector(".result-text .main-result");
 const guessTextDiv = document.querySelector(".result-text .guesses-text");
+const guessNum = document.querySelector(".guess-num");
 
 let randomNum = Math.floor(Math.random() * 100);
 let guessArr = [];
+let guessCount = 10;
+
+guessNum.innerText = guessCount;
+newgamebtn.disabled = true;
 
 submitBtn.addEventListener("click", () => {
-  console.log(randomNum);
-
   if (randomNumInput.value) {
     guessArr.push(randomNumInput.value);
-    guessTextDiv.innerText = `You guesses: ${guessArr.map((num) => num)}`;
+    guessTextDiv.innerText = `You guesses: ${guessArr.join(", ")}`;
     if (randomNum > randomNumInput.value) {
       resultTextDiv.innerText = `${randomNumInput.value} is too low!`;
     } else if (randomNum < randomNumInput.value) {
       resultTextDiv.innerText = `${randomNumInput.value} is too high!`;
     } else if (randomNum == randomNumInput.value) {
       resultTextDiv.innerText = `You got it in ${guessArr.length} guesses! The number was ${randomNum}.`;
-      newgamebtn.classList.add("start");
-      submitBtn.classList.add("end");
+      submitBtn.disabled = true;
+      newgamebtn.disabled = false;
     }
 
+    if (guessCount > 1) {
+      guessCount -= 1;
+      console.log(guessCount);
+      guessNum.innerText = guessCount;
+    } else {
+      resultTextDiv.innerText = "You failed, the game is over";
+      submitBtn.disabled = true;
+      newgamebtn.disabled = false;
+    }
     randomNumInput.value = "";
   }
 });
@@ -36,4 +48,5 @@ randomNumInput.addEventListener("keydown", function (e) {
 
 newgamebtn.addEventListener("click", () => {
   location.reload();
+  newgamebtn.disabled = true;
 });
